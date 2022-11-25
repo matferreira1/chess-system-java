e22de7f4fe6a7d844877767e72f435ec6e896516
Matheus Ferreira <200024949@aluno.unb.br>
2022-11-25 16:04:17 -0300

    Classe Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("ola");
+		Position pos = new Position(2, 2);
+		System.out.println(pos);
 
 	}
 
